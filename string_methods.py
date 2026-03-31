dc2111f30a83ea6b911c0e683d3425a4be8f4cbd
cxthya <calderonhernandezcinthya@gmail.com>
2026-03-31 11:07:05 -0400

    string methods
    2 tasks

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+'''
+Cinthya Calderon-Hernandez
+CMSC 111
+Spring 2026
+Assignment 3: Using string methods
+'''
+# Task 1
+text = "this is a simple test"
+#count occurrences of 's'
+count = text.count("s")
+print(f'The letter "s" appears {count} times in the text.')
+
+# Task 2
+text = "This is an old example."
+
+#Replace function
+updated_text = text.replace("old", "new")
+print(updated_text)
